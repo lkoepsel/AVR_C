64639b61b23f4ccf7b00a2a4d157473db3a027c4
Lief Koepsel <lkoepsel@wellys.com>
2021-10-19 06:57:34 -0700

    changed chario to serialio and updated the main.c code to mirror that of K&R example

---
+++ examples/serialio/main.c
@@ -0,0 +1,27 @@
+/*
+ * Serial I/O test
+ *
+ * Adapted from K&R The C Programming Language V3 page 17
+ *
+ *
+ */
+ 
+#include <stdio.h>
+#include "uart.h"
+
+int main(void) {    
+
+    uart_init();
+    stdout = &uart_output;
+    stdin  = &uart_input;
+                
+    char input;
+
+    
+    puts("Serial I/O Test");
+    while((input = getchar())!= EOF) {
+        printf("You entered %c\n", input);        
+    }
+        
+    return 0;
+}
