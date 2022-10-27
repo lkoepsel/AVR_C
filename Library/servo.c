899cda759eec1e5e3f3b7914ca1b5a5f81ead632
lief koepsel <lkoepsel@wellys.com>
2022-10-27 06:24:37 -0700

    confirmed servo code works, not yet, stable

--- Library/servo.c
+++ Library/servo.c
@@ -3,6 +3,7 @@
 #include "servo.h"
 
 const uint16_t servo_pulse_width = 1000;
+volatile uint8_t *DDRn;
 servo servos[MAX_SERVOS];
 
 void init_servos (void)
@@ -15,6 +16,7 @@ void init_servo(uint8_t index, uint8_t bit, volatile uint8_t *port, uint8_t stat
     uint16_t high_width)
 {
     servos[index].bit = bit;
+    DDRn = port;
     servos[index].port = port;
     servos[index].state = state;
     servos[index].high_width = high_width;
@@ -22,8 +24,10 @@ void init_servo(uint8_t index, uint8_t bit, volatile uint8_t *port, uint8_t stat
     servos[index].high_count = high_width;
     servos[index].low_count = servos[index].low_width;
 
-    // DDRn is 1 addr below PORTn, decrement and make a pointer to ref DDRn
-    set_bit(*--servos[index].port, servos[index].bit);
+    // DDRn is 1 addr below PORTn
+    // set bit in DDRn to enable pin to OUTPUT
+    DDRn--;
+    set_bit(*DDRn, servos[index].bit);
     return ;
 }
 
--- examples/multi_servo/main.c
+++ examples/multi_servo/main.c
@@ -7,12 +7,9 @@ int main (void)
 {
 
     // index, bit, port, state, high_width)
-    init_servo(0,2, &PORTD, LOW, 35);
-    init_servo(1,3, &PORTD, LOW, 80);
-    init_servo(2,4, &PORTD, LOW, 35);
-    init_servo(3,0, &PORTB, LOW, 80);
-    init_servo(4,1, &PORTB, LOW, 35);
-    init_servo(5,2, &PORTB, LOW, 80);
+    init_servo(0,2, &PORTD, LOW, 40);
+    init_servo(1,3, &PORTD, LOW, 60);
+    init_servo(2,4, &PORTD, LOW, 100);
 
     // Initialize servo clock to enable servos
     init_servos ();
