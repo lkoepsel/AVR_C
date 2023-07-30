2d5144fd01758046628fa54b258ae363b5bf18bc
Lief Koepsel <lkoepsel@wellys.com>
2023-07-30 15:35:52 -0700

    added serialio string showing issues with scanf and fgets and serialio readline to demo a good method

--- examples/serialio_string/main.c
+++ examples/serialio_string/main.c
@@ -1,4 +1,8 @@
-// Use scand to read a string from the serial console
+// Demonstrate the problems with scanf() and fgets() to read a string
+// from the serial console
+// scanf() will allow a buffer over-run, resulting in a crash
+// fgets() only allows a set number of char, limiting flexibility and overwrites
+// once it has read the alloted number of char
 
 #include <stdio.h>
 #include "uart.h"
@@ -6,46 +10,19 @@
 int main(void) {    
 
     init_serial();
-    
-    char input1[16];
-    uint8_t input2[16];
+    #define MAX_CHAR 8
+    char input1[MAX_CHAR] = {};
+    char input2[MAX_CHAR] = {};
 
-    puts("Serial I/O Test: String");
-    while(1) {
-        printf("Enter up to 8 char\n");
-        fgets(input1, 8, stdin);
+    puts("Serial I/O Test: scanf() and fgets()");
+    while(1)
+    {
+        printf("scanf(): Enter up to %i char\n", MAX_CHAR - 1);
+        scanf("%s", input1);
         printf("You entered %s\n", input1);
 
-        printf("Enter any number of char\n");
-        uint8_t i = 0;
-        uint8_t end = 1;
-        while (end)
-        {
-            input2[i] = getchar() - '0';
-
-            if (ferror(stdin))
-            {
-                printf("Error in reading char");
-                clearerr(stdin);
-                end = 0;
-            }
-            else if (feof(stdin))
-            {
-                printf("End of file encountered");
-                clearerr(stdin);
-                end = 0;
-            }
-            else
-                i++;
-            if (i > 10)
-            {
-                end = 0;
-            }
-            for (uint8_t j=0;j<10;j++)
-            {
-                printf("%i", input2[j]);
-            }
-            printf("\n");
-        }
-    }        
+        printf("fgets(): Enter %i char\n", MAX_CHAR - 1);
+        fgets(input1, MAX_CHAR, stdin);
+        printf("You entered %s\n", input1);
+    }
 }
