ea0ad8ec686f4ee9b2b0c0464914e66b51d9835f
Lief Koepsel <lkoepsel@wellys.com>
2024-06-24 16:17:12 -0700

    redid soft_serial example, works well

--- examples/soft_serial/main.c
+++ examples/soft_serial/main.c
@@ -2,20 +2,25 @@
 // Slow serial port, use for non-intensive serial interaction
 
 #include "soft_serial.h"
+#include "delay.h"
 
 int main(void) {
     // Initialize software serial and hardware serial (UART)
-    init_soft_serial();
     init_serial();
 
-    char letters[12] = {"Soft Serial"};
+    char soft_out[18] = {"Enter <11 chars: "};
+    char soft_in[11] = {""};
 
     // Example: Send and receive data
+    puts("Enter upto 10 characters on soft terminal");
+    init_soft_serial();
     while (1) {
-        soft_string_write(letters, (sizeof(letters)/init_soft_serial(letters[0])));
+        soft_string_write(soft_out, (sizeof(soft_out)/sizeof(soft_out[0])));
+
         // Receive data
-        uint8_t received = soft_readLine(letters, 4);
-        printf("n char: %i", received);
+        uint8_t received = soft_readLine(soft_in, 10);
+        printf("On soft term %i chars received\n", received);
+        printf("Chars are: %s\n", soft_in);
     }
 
     return 0;
