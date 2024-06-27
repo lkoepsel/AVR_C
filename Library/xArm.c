ccf32da87d6401a577e2cff88e6a326ebf04d573
Lief Koepsel <lkoepsel@wellys.com>
2024-06-27 08:41:42 -0700

    integrated all xArm CCoursen code into xArm, voltage still works

--- Library/xArm.c
+++ Library/xArm.c
@@ -3,7 +3,7 @@
 char xArm_in[xArm_MAX_BUFFER + 1] = {};
 char xArm_out[xArm_MAX_BUFFER + 1] = {};
 
-void xArm_send(int cmd, int len)
+void xArm_send(uint8_t cmd, uint8_t len)
 {
     putchar(SIGNATURE);
     putchar(SIGNATURE);
@@ -18,7 +18,7 @@ void xArm_send(int cmd, int len)
     }
 }
 
-int xArm_recv(int cmd)
+uint8_t xArm_recv(uint8_t cmd)
 {
   uint8_t num_char = readLine(xArm_in, 4);
   printf("num_char: %i", num_char);
@@ -32,7 +32,181 @@ int xArm_recv(int cmd)
   return -1;
 }
 
-int xArm_getBatteryVoltage()
+uint16_t xArm_clamp(uint16_t v) {
+    return (v < xArm_lo) ? xArm_lo : (xArm_hi < v) ? xArm_hi : v;
+}
+
+// void xArmServoController::setPosition(uint8_t servo_id, uint16_t position, uint16_t duration = 1000, bool wait = false)
+// {
+//   position = xArm_clamp(position);
+//   _buffer[0] = 1;
+//   _buffer[1] = lowByte(duration);
+//   _buffer[2] = highByte(duration);
+//   _buffer[3] = servo_id;
+//   _buffer[4] = lowByte(position);
+//   _buffer[5] = highByte(position);
+//   send(CMD_SERVO_MOVE, 6);
+//   if (wait) {
+//     delay(duration);
+//   }
+// }
+
+// void xArmServoController::setPosition(xArmServo servo, unsigned duration = 1000, bool wait = false)
+// {
+//   setPosition(servo.servo_id, servo.position, duration, wait);
+// }
+
+// void xArmServoController::setPosition(xArmServo servos[], int count, unsigned duration = 1000, bool wait = false)
+// {
+//   int len = count * 3 + 3;
+//   _buffer[0] = count;
+//   _buffer[1] = lowByte(duration);
+//   _buffer[2] = highByte(duration);
+//   for (int i = 3, j = 0; j < count; i += 3, j++) {
+//     _buffer[i] = servos[j].servo_id;
+//     _buffer[i + 1] = lowByte(servos[j].position);
+//     _buffer[i + 2] = highByte(servos[j].position);
+//   }
+//   send(CMD_SERVO_MOVE, len);
+//   if (wait) {
+//     delay(duration);
+//   }
+// }
+
+// /*** GetPosition ***/
+
+// int xArmServoController::getPosition(int servo_id)
+// {
+//   _buffer[0] = 1;
+//   _buffer[1] = servo_id;
+//   send(CMD_GET_SERVO_POSITION, 2);
+//   if (-1 != recv(CMD_GET_SERVO_POSITION)) {
+//     return _buffer[3] * 256 + _buffer[2];
+//   }
+//   return -1;
+// }
+
+// int xArmServoController::getPosition(xArmServo &servo)
+// {
+//   int pos = getPosition(servo.servo_id);
+//   servo.position = pos;
+//   return pos;
+// }
+
+// bool xArmServoController::getPosition(xArmServo servos[], int count)
+// {
+//   _buffer[0] = count;
+//   for (int i = 1, j = 0; j < count; i++, j++) {
+//     _buffer[i] = servos[j].servo_id;
+//   }
+//   send(CMD_GET_SERVO_POSITION, count + 1);
+//   // servo positions returned are in the same order sent.
+//   if (-1 != recv(CMD_GET_SERVO_POSITION)) {
+//     for (int i = 2, j = 0; j < count; i += 3, j++) {
+//       servos[j].position = _buffer[i + 1] * 256 + _buffer[i];
+//     }
+//     return true;
+//   }
+//   return false;
+// }
+
+// /*** Off ***/
+
+// void xArmServoController::servoOff(int servo_id)
+// {
+//   _buffer[0] = 1;
+//   _buffer[1] = servo_id;
+//   send(CMD_SERVO_STOP, 2);
+// }
+
+// void xArmServoController::servoOff(int num, int servo_id, ...)
+// {
+//   _buffer[0] = num;
+//   _buffer[1] = servo_id;  
+//   if (num > 1) {
+//     va_list args;
+//     va_start(args, servo_id);
+//     for (int i = 2; i <= num; i++) {
+//       _buffer[i] = (byte)va_arg(args, int);
+//     }
+//     va_end(args);    
+//   }
+//   send(CMD_SERVO_STOP, num + 1);
+// }
+
+// void xArmServoController::servoOff(xArmServo servo)
+// {
+//   servoOff(servo.servo_id);
+// }
+
+// void xArmServoController::servoOff(xArmServo servos[], int count)
+// {
+//   _buffer[0] = count;
+//   for (int i = 1, j = 0; j < count; i++, j++) {
+//     _buffer[i] = servos[j].servo_id;
+//   }
+//   send(CMD_SERVO_STOP, count + 1);
+// }
+
+// void xArmServoController::servoOff()
+// {
+//   _buffer[0] = 6;
+//   _buffer[1] = 1;
+//   _buffer[2] = 2;
+//   _buffer[3] = 3;
+//   _buffer[4] = 4;
+//   _buffer[5] = 5;
+//   _buffer[6] = 6;
+//   send(CMD_SERVO_STOP, 7);
+// }
+
+// /*** Action Group ***/
+
+//   void xArmServoController::actionRun(int group, unsigned times = 1) {
+//     _buffer[0] = group;
+//     _buffer[1] = lowByte(times);
+//     _buffer[2] = highByte(times);
+//     send(CMD_ACTION_GROUP_RUN, 3);
+//     actionRunning = true;
+//   }
+
+//   void xArmServoController::actionStop() {
+//     send(CMD_ACTION_GROUP_STOP, 0);
+//   }
+
+//   void xArmServoController::actionSpeed(int group, unsigned percent) {
+//     _buffer[0] = group;
+//     _buffer[1] = lowByte(percent);
+//     _buffer[2] = highByte(percent);
+//     bool _actionRunning = actionRunning;
+//     send(CMD_ACTION_GROUP_SPEED, 3);
+//     actionRunning = _actionRunning;
+//   }
+
+//   bool xArmServoController::actionIsRunning() {
+//     return actionRunning;
+//   }
+
+//   bool xArmServoController::serialEvent() {
+//     if (actionRunning) {
+//       if (-1 != serial_port.readBytes(_buffer, 4)) {
+//         if (_buffer[0] == SIGNATURE && _buffer[1] == SIGNATURE) {
+//           switch (_buffer[3])
+//           {
+//           case CMD_ACTION_GROUP_STOP:
+//           case CMD_ACTION_GROUP_END:
+//             actionRunning = false;
+//             return true;
+//           default:
+//             break;
+//           }
+//         }
+//       }
+//     }
+//     return false;
+//   }
+
+uint16_t xArm_getBatteryVoltage()
 {
   xArm_send(CMD_GET_BATTERY_VOLTAGE, 0);
   int8_t results = xArm_recv(CMD_GET_BATTERY_VOLTAGE);
@@ -40,4 +214,8 @@ int xArm_getBatteryVoltage()
     return (xArm_in[1] << 8) + xArm_in[0];
   }
   return results;
+}
+
+void xArm_beep() {
+  xArm_send(CMD_BEEP, 1);
 }
--- Library/xArm.h
+++ Library/xArm.h
@@ -1,16 +1,19 @@
-// xArm getVoltage
-// Change baud rate in env.make to 9600 to match xArm
+// xArm ServoController C99 version
+// Copied from https://github.com/ccourson/xArmServoController
 // 
+//   xArmServoController.h - Library for controlling xArm servos.
+//   Version 1.0.0
+//   Created by Chris Courson, July 9, 2020.
 // 
 
 #ifndef XARM_H
 #define XARM_H
 
+#include <stdbool.h>
 #include <stdio.h>
-#include "uart.h"
 #include <string.h>
 #include "readLine.h"
-
+#include "uart.h"
 
 #define SIGNATURE               0x55
 #define CMD_BEEP                0x00
@@ -23,14 +26,46 @@
 #define CMD_SERVO_STOP          0x14
 #define CMD_GET_SERVO_POSITION  0x15
 
+// replace modal clamp limits, as this is only for xArm
+#define xArm_lo 0 
+#define xArm_hi 1000
+
+struct xArmServo {
+    uint8_t servo_id;
+    uint16_t position;
+};
+
 #define xArm_MAX_BUFFER 32
 extern char xArm_in[xArm_MAX_BUFFER + 1];
 extern char xArm_out[xArm_MAX_BUFFER + 1];
 
-void xArm_send(int cmd, int len);
+// void setPosition(uint8_t servo_id, uint16_t position, uint16_t duration = 1000, bool wait = false);
+// void setPosition(xArmServo servo, uint16_t duration = 1000, bool wait = false);
+// void setPosition(xArmServo servos[], uint8_t count, uint16_t duration = 1000, bool wait = false);
 
-int xArm_recv(int cmd);
+// uint16_t getPosition(uint8_t servo_id);
+// uint16_t getPosition(xArmServo &servo);
+// bool getPosition(xArmServo servos[], uint8_t count);
 
-int xArm_getBatteryVoltage();
+// void servoOff(uint8_t servo_id);
+// void servoOff(int num, uint8_t servo_id, ...);
+// void servoOff(xArmServo servo);
+// void servoOff(xArmServo servos[], uint8_t count);
+// void servoOff();
+
+// void actionRun(uint8_t group, uint8_t times = 1);
+// void actionStop();
+// void actionSpeed(uint8_t group, uint8_t percent);
+// bool actionIsRunning();
+// bool serialEvent();
+
+uint16_t xArm_getBatteryVoltage();
+void xArm_beep();
+
+bool actionRunning;
+
+uint16_t clamp(uint16_t v);
+void xArm_send(uint8_t cmd, uint8_t len);
+uint8_t xArm_recv(uint8_t cmd);
 
 #endif
